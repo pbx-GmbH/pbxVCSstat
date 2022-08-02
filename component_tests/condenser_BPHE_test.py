ac230162dfa3e73f77e923a1d14522afb039246d
dradler-pbx <dominik.radler@productbloks.com>
2022-08-02 14:41:56 +0200

    backup commit

--- component_tests/condenser_BPHE_test.py
+++ component_tests/condenser_BPHE_test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import VCSpbx as vcs
 from CoolProp.CoolProp import PropsSI as CPPSI
 
@@ -14,7 +16,7 @@ mdot_SL = 0.5  # mass flow coolant
 p_SL = 1e5  # pressure coolant
 
 # components
-system = vcs.System(id='system', tolerance=1e-4)
+system = vcs.System(id='system', tolerance=1e-8)
 cond = vcs.CondenserBPHE(id='cond', system=system, k=[300., 300., 300.], area=0.8, subcooling=0.1)
 srcSL = vcs.Source(id='src_SL', system=system, mdot=mdot_SL, p=p_SL, h=h_SL_in)
 snkSL = vcs.Sink('snk_SL', system)
@@ -28,14 +30,23 @@ srcSL_cond = vcs.Junction('srcSL_cond', system, SL, srcSL, 'outlet_A', cond, 'in
 cond_snkSL = vcs.Junction('cond_snkSL', system, SL, cond, 'outlet_B', snkSL, 'inlet_A', mdot_SL, p_SL, h_SL_in)
 
 
-system.run(full_output=True)
+# system.run(full_output=True)
 #
 # system.initialize()
-cond.calc()
+# cond.calc()
+#
+# Qdot_refside = (srcRef_cond.get_enthalpy() - cond_snkRef.get_enthalpy()) * srcRef_cond.get_massflow()
+# Qdot_coolantside = (srcSL_cond.get_enthalpy() - cond_snkSL.get_enthalpy()) * srcSL_cond.get_massflow()
+# print('Energy balance:')
+# print('Qdot_refside = {}'.format(Qdot_refside))
+# print('Qdot_coolantside = {}'.format(Qdot_coolantside))
+# print('Difference = {}'.format(Qdot_refside+Qdot_coolantside))
 
-Qdot_refside = (srcRef_cond.get_enthalpy() - cond_snkRef.get_enthalpy()) * srcRef_cond.get_massflow()
-Qdot_coolantside = (srcSL_cond.get_enthalpy() - cond_snkSL.get_enthalpy()) * srcSL_cond.get_massflow()
-print('Energy balance:')
-print('Qdot_refside = {}'.format(Qdot_refside))
-print('Qdot_coolantside = {}'.format(Qdot_coolantside))
-print('Difference = {}'.format(Qdot_refside+Qdot_coolantside))
+mdot_ref_range = np.arange(0.01, 0.05, 0.005)
+results = {'Qdot_refside': [], 'Qdot_coolantside': [], 'pC': []}
+for mdot_ref_set in mdot_ref_range:
+    srcRef.set_mdot(mdot_ref_set)
+    system.run()
+    results['Qdot_refside'].append((srcRef_cond.get_enthalpy() - cond_snkRef.get_enthalpy()) * srcRef_cond.get_massflow())
+    results["Qdot_coolantside"].append((srcSL_cond.get_enthalpy() - cond_snkSL.get_enthalpy()) * srcSL_cond.get_massflow())
+    results['pC'].append(cond_snkRef.get_pressure())
